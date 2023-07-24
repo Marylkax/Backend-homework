ebb279993ce5000dd7d2d6c98970768272ea81df
Marylkax <108342249+Marylka@users.noreply.github.com>
2023-07-24 07:59:26 +0100

    SQL addition

--- routes/get.js
+++ routes/get.js
@@ -1,33 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const asyncMySQL = require("../mySQL/connection");
 
-router.get("/characters", (req, res) => {
-  //   if (!characters) {
-  //     res.send({ status: 0, reason: "ID not found" });
-  //   }
-  // was trying to check chanracters spelt correctly? is there a way for this
-  res.send({ status: 1, characters: req.rickandmorty });
-});
+// router.get("/characters", (req, res) => {
+//   res.send({ status: 1, characters: req.rickandmorty });
+// });
 
-router.get("/character/:id", (req, res) => {
-  //defensive check for id valid number
-  if (!Number(req.params.id)) {
-    res.send({ status: 0, reason: "Invalid ID, please enter a number" });
-    return;
-  }
-
-  //copy and find a character
-  const _rickandmorty = [...req.rickandmorty];
-  const character = _rickandmorty.find((char) => {
-    return char.id === Number(req.params.id);
-  });
-
-  //if no id match, return message to user
-  if (!character) {
-    res.send({ status: 0, reason: "ID not found" });
-  }
-
-  res.send({ status: 1, character });
+router.get("/character/:id", async (req, res) => {
+  const results = asyncMySQL(
+    `SELECT name, species, origin, gender FROM rickandmorty WHERE id LIKE ${id};`
+  );
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const app = express();
-const rickandmorty = require("./rickandmorty.json");
-
-// already contains ids
 
 //see requests to backend on console
 app.use((req, res, next) => {
@@ -10,25 +7,14 @@ app.use((req, res, next) => {
   next();
 });
 
-//now available to the whole programme
-app.use((req, res, next) => {
-  req.rickandmorty = rickandmorty;
-  next();
-});
-
 app.use(express.json());
-
-// app.get("/homework", (req, res) => {
-//   console.log("New request from the front");
-//   res.send("Hello from backend");
-// });
 
 app.use("/get", require("./routes/get"));
 //currently have 20 characters stored in rickandmort.json
 
-app.use("/delete", require("./routes/delete"));
-app.use("/add", require("./routes/add"));
-app.use("/update", require("./routes/update"));
+// app.use("/delete", require("./routes/delete"));
+// app.use("/add", require("./routes/add"));
+// app.use("/update", require("./routes/update"));
 
 const port = process.env.PORT || 6001;
 app.listen(port, () => {
